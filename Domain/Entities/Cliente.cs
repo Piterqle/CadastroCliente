b6944dc39d73e4cfb9818ef689387969c4c42c18
Petrarca-User <Petrarca-User@PETRARCA>
2026-06-22 09:16:57 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroCliente.Domain.Entities
+{
+    internal class Cliente
+    {
+        public Guid idCliente { get; private set; }
+
+
+    }
+}
